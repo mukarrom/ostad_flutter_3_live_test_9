aec8e9fc787085f5fc82de87ac9acb16476c25d8
mukarrom <mukarrom.hosain.1@gmail.com>
2023-10-06 23:14:10 +0600

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_flutter_3_livetest_9/food_recipes.dart';
-import 'package:ostad_flutter_3_livetest_9/recipes_model.dart';
+
+Map recipes = {
+  "recipes": [
+    {
+      "title": "Pasta Carbonara",
+      "description": "Creamy pasta dish with bacon and cheese.",
+      "Ingredients": ["spaghetti", "bacon", "+88", "cheese"]
+    },
+    {
+      "title": "Caprese Salad",
+      "description":
+          "Simple and refreshing salad with tomatoes, mozzarella, and basil.",
+      "Ingredients": ["tomatoes", "nozzarella", "basil"]
+    },
+    {
+      "title": "Banana Smoothie",
+      "description": "Healthy and creamy smoothie with bananas and milk.",
+      "Ingredients": ["bananas", "nilk"]
+    },
+    {
+      "title": "Chicken Stir-Fry",
+      "description": "Quick and flavorful stir-fried chicken with vegetables.",
+      "Ingredients": ["chicken breast", "broccols", "carrot", "soy sauce"]
+    },
+    {
+      "title": "Grilled Salmon",
+      "description": "Delicious grilled salmon with lemon and herbs.",
+      "Ingredients": ["salmon fillet", "lemon", "olive oil", "1"]
+    },
+    {
+      "title": "Vegetable Curry",
+      "description": "Spicy and aromatic vegetable curry.",
+      "Ingredients": ["mixed vegetables", "coconut milk", "curry powder"]
+    },
+    {
+      "title": "Berry Parfait",
+      "description": "Layered dessert with fresh berries and yogurt.",
+      "Ingredients": ["berries", "yogurt", "granola"]
+    }
+  ]
+};
 
 void main() {
   runApp(const MyApp());
@@ -56,6 +95,22 @@ class _MyHomePageState extends State<MyHomePage> {
           );
         },
       ),
+    );
+  }
+}
+
+class Food {
+  final String title, description;
+  final List<String> ingredients;
+
+  Food(this.title, this.description, this.ingredients);
+
+// factory method
+  factory Food.toJson(Map<String, dynamic> e) {
+    return Food(
+      e['title'],
+      e['description'],
+      e['Ingredients'],
     );
   }
 }
